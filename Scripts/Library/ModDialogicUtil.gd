8df54e9ab2662ffa4b0e8982185435d30b126f15
Aaron Worsham <aaronworsham@gmail.com>
2024-11-16 12:57:55 -0500

    got diaalogic back up and running

--- Scripts/Library/ModDialogicUtil.gd
+++ Scripts/Library/ModDialogicUtil.gd
@@ -5,9 +5,14 @@ class_name MoDDialogicUtil extends Node
 static func parse_signal_key(key:String)->Dictionary:
 	var dict:Dictionary
 	var key_array:Array = key.split("/",true,0)
-	dict["interactable"] = key_array[0]
-	dict["category"] = key_array[1]
-	dict["category_key"] = key_array[2]
-	dict["sub_category"] = key_array[3]
-	dict["sub_key"] = key_array[4]
+	if key_array.size() >= 1 :
+		dict["interactable"] = key_array[0]
+	if key_array.size() >= 2 :
+		dict["category"] = key_array[1]
+	if key_array.size() >= 3 :
+		dict["category_key"] = key_array[2]
+	if key_array.size() >= 4 :
+		dict["sub_category"] = key_array[3]
+	if key_array.has() :
+		dict["sub_key"] = key_array[4]
 	return dict
