172393d7d9db10bd0009ebc7e18c26e71d7ff52e
Aaron Worsham <aaronworsham@gmail.com>
2024-11-23 11:14:10 -0500

    got python script working

--- Scripts/Python/UpdateJson.py
+++ Scripts/Python/UpdateJson.py
@@ -1,10 +1,20 @@
 
+import json
+import urllib
+from urllib.request import urlopen
 
-import requests 
-   
-# Making a GET request 
-r = requests.get('http://localhost:1337/api/mysteries') 
-  
 
-data = r.json()
-print(data['data'][0]["guid"])
+full_url = 'http://localhost:1337/api/mysteries?populate[0]=state&populate[1]=clues&populate[2]=clues.actor&populate[3]=clues.state'
+
+r =  urlopen(full_url)
+
+bdata = r.read()
+sdata = bdata.decode("utf-8")
+
+print(sdata)
+
+jdata = json.loads(sdata)
+
+with open("/home/aaron/Godot/mid_of_night_godot/Tests/test_mysteries_strapi.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(jdata, write_file)
+    
