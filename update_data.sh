0af9eb47eced093be636d29b52b0d431c9072394
Aaron Worsham <aaronworsham@gmail.com>
2024-11-24 09:56:38 -0500

    add strapi export to bash script

--- update_data.sh
+++ update_data.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 
-#! /bin/bash
 python3 /home/aaron/Godot/mid_of_night_godot/Scripts/Python/UpdateJson.py
 "/usr/local/bin/godot-engine" -s res://addons/gut/gut_cmdln.gd  -d --headless
+npm run strapi export  --prefix /home/aaron/Applications/Strapi -- --no-encrypt --file /home/aaron/Godot/mid_of_night_godot/strapi_export_file
