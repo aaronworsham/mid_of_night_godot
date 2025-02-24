b051e91f81ace1fc7ae6f5d1dcab6e4798f753c4
Aaron Worsham <aaronworsham@gmail.com>
2025-02-24 09:18:00 -0500

    More work with inventory

--- Scripts/Systems/Inventory/InventoryController.gd
+++ Scripts/Systems/Inventory/InventoryController.gd
@@ -1,12 +1,14 @@
 class_name InventoryController extends Node
 
-
+var _items_resource:ItemsResource
 @export var _inventory:Array
 
 func _ready() -> void:
-    _inventory.clear()
+    on_load()
 
 func on_load() -> void:
+    _items_resource = ItemsResource.new()
+    _items_resource.on_load()
     _inventory.clear()
 
 func add_to_inventory(i:ItemResource) -> void:
@@ -15,6 +17,8 @@ func add_to_inventory(i:ItemResource) -> void:
 func get_inventory() -> Array:
     return _inventory
 
+func get_items()-> Array:
+    return _items_resource.get_items_as_resources()
 
 func _on_inventory_btn_pressed() -> void:
     pass # Replace with function body.
--- Tests/Systems/TestInventorySystem.gd
+++ Tests/Systems/TestInventorySystem.gd
@@ -1,8 +1,5 @@
 class_name TestInventorySystem extends GutTest
 
-var ic:InventoryController = InventoryController.new()
-
-
 func test_item_resource():
     var ir:ItemResource = ItemResource.new()
     ir.on_load()
@@ -19,14 +16,17 @@ func test_item_resource():
     assert_not_null(ir.get_guid())
     assert_not_same(ir.get_guid(), "")
 
-func test_add_to_inventory():
-    assert_eq(ic.get_inventory().size(), 0)
-    var ir:ItemResource = ItemResource.new()
-    ic.add_to_inventory(ir)
-    assert_eq(ic.get_inventory().size(), 1)
-
 func test_items_resource():
     var ir:ItemsResource = ItemsResource.new()
     ir.on_load()
     assert_gt(ir.get_items().size(), 0)
     assert_gt(ir.get_items_as_resources().size(), 0)
+
+func test_items_controller():
+    var ic:InventoryController = InventoryController.new()
+    ic.on_load()
+    assert_gt(ic.get_items().size(), 0)
+    assert_eq(ic.get_inventory().size(), 0)
+    var ir:ItemResource = ItemResource.new()
+    ic.add_to_inventory(ir)
+    assert_eq(ic.get_inventory().size(), 1)
